1d7500bc6b86e2b36524fcddb9964e5060545089
Kembly Quirós Araya <kquirosaraya@gmail.com>
2018-04-05 21:09:25 -0500

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,19 +6,9 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-
-  constructor(private http: HttpClient){}
-
-  ngOnInit(): void {
-    this.http.get('https://api.github.com/users/kemblyqa')
-    .subscribe(
-      data => {
-        console.log(data);
-      },
-      err => {
-        console.log("Error")
-      }
-    );
+export class LoginComponent {
+  constructor(){
+    
   }
+
 }
--- src/app/models/fetch.model.ts
+++ src/app/models/fetch.model.ts
@@ -1,3 +1,17 @@
-getResponse(){
-    
+import { HttpClient } from '@angular/common/http';
+
+export class Service {
+    constructor(private http: HttpClient){}
+
+    getData(apiUrl: string){
+        this.http.get(apiUrl)
+            .subscribe(
+            data => {
+                console.log(data);
+            },
+            err => {
+                console.log("Error")
+            }
+        );        
+    }
 }
