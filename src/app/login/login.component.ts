ebb933afda2d2715dd401d8fce58342cbf892129
Kembly Quirós Araya <kquirosaraya@gmail.com>
2018-04-24 12:01:35 -0500

    Internal comments on login and main menu

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,12 +14,14 @@ declare var $ : any;
 })
 
 export class LoginComponent{
+  /* variables needed for binding user data */
   nickname:string
   details:string
   errorMsg:any
 
+  /* constructor implements service: for request, router: for navigate */
   constructor(private service: Service, private router: Router){}
-
+  /* login to menu, check the user, if doesnt exists, create a new account for him */
   login(){
     this.service.getData("/user/checkUsuario",{
       params: {idUsuario: document.getElementById("txtEmail").textContent}
@@ -38,11 +40,11 @@ export class LoginComponent{
         }
       )
   }
-
+  /* register modal, to register a new user */
   register(){
     $('#register').modal('show')
   }
-
+  /* register request, to register a new user */
   registerUser(){
     this.service.postData("/user/crearUsuario",{
       idUsuario: document.getElementById("txtEmail").textContent,
@@ -56,12 +58,12 @@ export class LoginComponent{
         }
       )
   }
-
+  /* alert modal, if something is wrong, shows the possible error */
   alertUser(data: any){
     this.errorMsg = data
     $("#failed").modal("show")
   }
-
+  /* routes the user to main menu view */
   routeTo(){
     UserDetails.Instance.setActive(document.getElementById("txtEmail").textContent,this.nickname,this.details,document["imgPerfil"].src)
     this.router.navigate(['/menu'])
